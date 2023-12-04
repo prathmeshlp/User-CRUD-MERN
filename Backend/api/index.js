2843c9b0a4ac56f52ee6a17bfb548499abcb392b
Prathmesh Mulhar <prathmeshmulhar@gmail.com>
2023-12-04 13:45:03 +0530

    added vercel

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const UserRoutes = require("./routes/userRoutes");
+const UserRoutes = require("../routes/userRoutes");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
